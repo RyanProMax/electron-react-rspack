e18837b4bde20505da1d2e7a41e6c05818f79b44
Ryan <627311610@qq.com>
2023-10-04 00:58:54 +0800

    fix: expose ipcRenderer

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,25 +1,7 @@
-import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
-import { Channels } from '../common/constant';
+import { contextBridge, ipcRenderer } from 'electron';
 
 const electronHandler = {
-  ipcRenderer: {
-    invoke(channel: Channels, ...args: unknown[]) {
-      return ipcRenderer.invoke(channel, ...args);
-    },
-    send(channel: Channels, ...args: unknown[]) {
-      return ipcRenderer.send(channel, ...args);
-    },
-    on(channel: Channels, subscription: (_event: IpcRendererEvent, ...args: unknown[]) => void) {
-      ipcRenderer.on(channel, subscription);
-
-      return () => {
-        ipcRenderer.removeListener(channel, subscription);
-      };
-    },
-    once(channel: Channels, subscription: (_event: IpcRendererEvent, ...args: unknown[]) => void) {
-      ipcRenderer.once(channel, subscription);
-    },
-  },
+  ipcRenderer,
 };
 
 contextBridge.exposeInMainWorld('__ELECTRON__', electronHandler);
